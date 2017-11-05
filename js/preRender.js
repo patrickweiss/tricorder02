8bde8eaafcddf75688387e3f48cf38ba3460957a
Patrick Weiss <patrick.weiss@officeone.team>
2017-11-05 15:18:20 +0000

    Google Drive Datei fuer Belege wird mit Metadaten angelegt, aber ist noch leer

--- js/preRender.js
+++ js/preRender.js
@@ -1,39 +1,90 @@
-  //we can later change to indexedDB
 
+// Client ID and API key from the Developer Console
+var CLIENT_ID = '745028189108-k9dk9mp8ttpu3orj3p4brsro3btb68u4.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyBeigsvPpOn9AUPNPUP-VYETf2tJDaAihw';
 
-  function loadState() {
-    var triStateJSON = localStorage.getItem("triStateJSON");
-  //  console.log("String aus localStorage:"+triStateJSON);
-    var triState = {};
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
 
-    if (triStateJSON == null) {
-      //Initialzustand herstellen
-      triState["Gewinn"] = "Yipi- Gewinn";
-      triState["Ausgaben"] = {};
-   
-       triStateJSON = JSON.stringify(triState);
-    localStorage.setItem("triStateJSON", triStateJSON);
-    }else   triState = JSON.parse(triStateJSON);
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.appdata';
 
-  //  console.log("triState Objekt:"+JSON.stringify(triState));
-       return triState;
+/*global gapi*/
+/**
+*  On load, called to load the auth2 library and API client library.
+*/
+ 
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+  
+}
+
+/**
+ *  Initializes the API client library and sets up sign-in state
+ *  listeners.
+ */
+function initClient() {
+  gapi.client.init({
+    apiKey: API_KEY,
+    clientId: CLIENT_ID,
+    discoveryDocs: DISCOVERY_DOCS,
+    scope: SCOPES
+  }).then(function () {
+    // Listen for sign-in state changes.
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+    // Handle the initial sign-in state.
+    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    //authorizeButton.onclick = handleAuthClick;
+    //signoutButton.onclick = handleSignoutClick;
+  });
+}
+
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+function updateSigninStatus(isSignedIn) {
+  console.log("Google login Status:"+isSignedIn);
+  if (!isSignedIn)gapi.auth2.getAuthInstance().signIn();
+}
+
+  
+ /*global localStorage*/ 
+class OOBusinessObjects{
+  static getByOfficeOneIDSpreadsheetNameSheetName(folderId,spreadsheetName,sheetName){
+    var kontenArray = [
+      { ID: "Porto", Konto: "GUV", MwSt: "7%" },
+      { ID: "Benzin", Konto: "GUV", MwSt: "19%" },
+      { ID: "Hotel", Konto: "GUV", MwSt: "19%" }
+    ]
+    return kontenArray;
   }
-
-
-  function storeState(state) {
-    triStateJSON = JSON.stringify(state);
-  //  console.log("State in localStorage speichern:"+triStateJSON);
-    localStorage.setItem("triStateJSON", triStateJSON);
+  static addAusgabe(buchungsperiode,betrag,konto,belegurl){
+    var ausgabenArray=JSON.parse(localStorage.getItem("ausgabe"+buchungsperiode));
+    if (!ausgabenArray)ausgabenArray=[];
+    var neueAusgabe={buchungsperiode:buchungsperiode,betrag:betrag,konto:konto,belegurl:belegurl};
+    ausgabenArray.push(neueAusgabe);
+    localStorage.setItem("ausgabe"+buchungsperiode,JSON.stringify(ausgabenArray));
+  }
+  static saveFile(metadata,file,folderId){
+     console.log("Beleg in GoogleDrive speichern");
+     
+     
+     var fileMetadata = {
+       "name": metadata.name,
+       parents:[folderId],
+       description:JSON.stringify(metadata)
+     };
+  
+     gapi.client.drive.files.create({
+       resource: fileMetadata,
+       fields: 'id'
+     }).then(function(response) {
+       console.log(response);
+     });
+               
   }
   
-  
-  class OOBusinessObjects{
-    static getByOfficeOneIDSpreadsheetNameSheetName(folderId,spreadsheetName,sheetName){
-      var kontenArray = [
-        { ID: "Porto", Konto: "GUV", MwSt: "7%" },
-        { ID: "Benzin", Konto: "GUV", MwSt: "19%" },
-        { ID: "Hotel", Konto: "GUV", MwSt: "19%" }
-      ]
-      return kontenArray;
-    }
-  }
+}
